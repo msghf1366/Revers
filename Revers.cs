38cb9a403d5da6b800fb57583bd4db816c6df370
msghf1366 <34458972+msghf1366@users.noreply.github.com>
2017-12-12 00:35:33 +0330

    Create Revers.cs
    hi
    program command line run
    write by visual studio 
    program language c#

---
+++ Revers.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string Read=Console.ReadLine();
+             for (int i = Read.Length - 1; i >=0; i--) Console.Write(Read[i]);
+             Console.ReadKey();
+        }
+        
+
+    }
+}
